403056cdfd0494bdac4da57cb8721cb6be24adc6
Adrian Ross <amr424@lehigh.edu>
2023-12-21 21:09:29 -0600

    update

--- src/app.ts
+++ src/app.ts
@@ -14,29 +14,13 @@ const ossProjectRepos = [
   "adrianmross",
 ];
 
+const baseUrl = "https://";
 const githubUsername = "adrianmross";
-// const websiteUrl = "https://bolajiayodeji.com";
-// const blogUrl = "https://blog.bolajiayodeji.com";
-// const newsletterUrl = "https://bawd.bolajiayodeji.com";
-// const youtubeUrl = "https://youtube.com/c/bolajiayodeji";
-// const slidesUrl = "https://slides.com/bolajiayodeji";
-// const twitterUrl = "https://twitter.com/iambolajiayo";
-// const linkedinUrl = "https://linkedin.com/in/iambolajiayo";
-// const githubSponsorsUrl = "https://github.com/sponsors/BolajiAyodeji";
-// const patreonUrl = "https://patreon.com/bolajiayodeji";
+const githubUrl = `${baseUrl}github.com/${githubUsername}`;
 
 async function generateMarkdown() {
-//   const websiteBadge = `[![Website Badge](https://img.shields.io/badge/-Website-3B7EBF?style=for-the-badge&logo=amp&logoColor=white)](${websiteUrl})`;
-//   const hashnodeBadge = `[![Blog Badge](https://img.shields.io/badge/-Blog-3B7EBF?style=for-the-badge&logo=Hashnode&logoColor=white)](${blogUrl})`;
-//   const substackBadge = `[![Newsletter Badge](https://img.shields.io/badge/-Newsletter-3B7EBF?style=for-the-badge&logo=Substack&logoColor=white)](${newsletterUrl})`;
-//   const youtubeBadge = `[![YouTube Badge](https://img.shields.io/badge/-Youtube-3B7EBF?style=for-the-badge&logo=Youtube&logoColor=white)](${youtubeUrl})`;
-//   const slidesBadge = `[![Slides Badge](https://img.shields.io/badge/-Slides-3B7EBF?style=for-the-badge&logo=slides&logoColor=white)](${slidesUrl})`;
-//   const linkedinBadge = `[![Linkedin Badge](https://img.shields.io/badge/-LinkedIn-3B7EBF?style=for-the-badge&logo=Linkedin&logoColor=white)](${linkedinUrl})`;
-//   const twitterBadge = `[![Twitter Badge](https://img.shields.io/badge/-@iambolajiayo-3B7EBF?style=for-the-badge&logo=x&logoColor=white)](${twitterUrl})`;
-//   const githubSponsorsBadge = `[![GitHub Sponsors Badge](https://img.shields.io/badge/-github%20sponsors-3B7EBF?style=for-the-badge&logo=github&logoColor=white)](${githubSponsorsUrl})`;
-//   const patreonBadge = `[![Patreon Badge](https://img.shields.io/badge/-Patreon-3B7EBF?style=for-the-badge&logo=Patreon&logoColor=white)](${patreonUrl})`;
 
-  const githubBadge = `[![GitHub Badge](https://img.shields.io/badge/-@adrianmross-%2312100E?style=flat-square&logo=GitHub&logoColor=white&link=https://www.github.com/adrianmross/)](https://www.github.com/adrianmross/)`;
+  const githubBadge = `[![GitHub Badge](https://img.shields.io/badge/-@adrianmross-%2312100E?style=flat-square&logo=GitHub&logoColor=white&link=${githubUrl})](${githubUrl})`;
   const linkedinBadge = `[![Linkedin Badge](https://img.shields.io/badge/-@adrianmross-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/adrianmross/)](https://www.linkedin.com/in/adrianmross/)`;
   const instaBadge = `[![Instagram Badge](https://img.shields.io/badge/-@adrian_m_ross-e1306c?style=flat-square&labelColor=e1306c&logo=instagram&logoColor=white&link=https://www.instagram.com/adrian_m_ross/)](https://www.instagram.com/adrian_m_ross/)`;
   const twitterBadge = `[![Twitter Badge](https://img.shields.io/badge/-@adrianmross-1ca0f1?style=flat-square&labelColor=1ca0f1&logo=twitter&logoColor=white&link=https://twitter.com/adrianmross)](https://twitter.com/adrianmross)`;
@@ -47,28 +31,39 @@ async function generateMarkdown() {
 
   const markdownText = `
 
-  ### Hi there 👋
-  <p>\n
-  - 🔭 I’m currently working on a B.S. degree in Computer Science and Business @ Lehigh University.\n
-  - 🌱 I’m currently learning about Decentralized Identifiers, Hierarchical Consensus for Blockchain Systems, and Scalable Systems.\n
-  - 💬 Ask me about the Lehigh CSB program or blockchain opportunities at Lehigh.\n
-  - 📫 How to reach me: [LinkedIn](https://www.linkedin.com/in/adrianmross/) @adrianmross\n
-  - 🌎 Where I've lived: 🇺🇸 (OH, NY, NJ, KS, PA), USA; 🇨🇿 Praha, Czech Republic; 🇨🇭 Basel, Switzerland;\n
-  - ⚡ Hobbies : ☕ Making Coffee, ⛷️ Skiing, 🎹 Piano
-  </p>\n
+  ### Hi there 👋\n
+
+  <!--
+  **adrianmross/adrianmross** is a ✨ _special_ ✨ repository because its README.md (this file) appears on your GitHub profile.
+  Here are some ideas to get you started:
+  - 🔭 I’m currently working on ...
+  - 🌱 I’m currently learning ...
+  - 👯 I’m looking to collaborate on ...
+  - 🤔 I’m looking for help with ...
+  - 💬 Ask me about ...
+  - 📫 How to reach me: ...
+  - 😄 Pronouns: ...
+  - ⚡ Fun fact: ...
+  -->
+
+  <ul>
+    <li>I'm currently working on a B.S. degree in Computer Science and Business @ Lehigh University.</li>
+    <li>I’m currently learning about Decentralized Identifiers, Hierarchical Consensus for Blockchain Systems, and Scalable Systems.</li>
+    <li>Ask me about the Lehigh CSB program or blockchain opportunities at Lehigh.</li>
+    <li>How to reach me: <a href="https://www.linkedin.com/in/adrianmross/">LinkedIn</a> @adrianmross</li>
+    <li>Where I've lived: 🇺🇸 (OH, NY, NJ, KS, PA), USA; 🇨🇿 Praha, Czech Republic; 🇨🇭 Basel, Switzerland;</li>
+    <li>Hobbies : ☕ Making Coffee, ⛷️ Skiing, 🎹 Piano</li>
+  </ul>\n
   ---\n
-
-  ${githubBadge} ${linkedinBadge} ${instaBadge} ${twitterBadge} \n
-
-  ---\n
-
   <div align="center">\n
+  ${githubBadge} ${linkedinBadge} ${instaBadge} ${twitterBadge} \n
 
   ${githubStatsCardDark}\n
   ${githubStatsCardLight}\n
 
   </div>\n
 
+  <!--
   ---\n
 
   ## Highlights
@@ -81,7 +76,9 @@ async function generateMarkdown() {
   ${await fetchGitHubData(ossProjectRepos)}\n
   </details>\n
 
-  ---\n`;
+  ---\n
+  -->
+  `;
 
   const result = md.render(markdownText);
 
